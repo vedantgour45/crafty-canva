4bc40e3d4c557eebbac4aadc8730ca7f14cb400f
Vedant Gour <vedantgour45@gmail.com>
2024-04-07 11:35:26 +0530

    inital commit

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Sidebar from "@/components/Shared/Sidebar";
+import MobileNav from "@/components/Shared/MobileNav";
+
+const Layout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <main className="root">
+      <Sidebar />
+      <MobileNav/>
+
+      <div className="root-container">
+        <div className="wrapper">{children}</div>
+      </div>
+    </main>
+  );
+};
+
+export default Layout;
